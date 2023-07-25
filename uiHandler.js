b7ffff15f58cf30c01321c22da9ec1007d272b29
unknown <a97tulsani@gmail.com>
2023-07-25 15:53:53 +0530

    3 peers video call

--- uiHandler.js
+++ uiHandler.js
@@ -2,6 +2,7 @@ import * as state from './state.js';
 
 
 const remoteViewPlayer = document.getElementById('remote-view');
+const remoteViewTwoPlayer = document.getElementById('remote-view-two');
 const localViewPlayer = document.getElementById('local-view');
 const remoteArena = document.getElementById('remote-arena')
 
@@ -10,23 +11,31 @@ export let setlocalViewSrc = (stream)=>{
 }
 
 export let setRemoteViewSrc = (stream)=>{
+    remoteViewPlayer.srcObject = streamToShow;
      
     let audioTracks = stream.getAudioTracks();
     let videoTracks = stream.getVideoTracks();
+
     console.log("Video Tracks",videoTracks);
         const streamToShow = new MediaStream();
         streamToShow.addTrack(videoTracks[videoTracks.length-1]);
+        remoteViewPlayer.srcObject= streamToShow;
         //streamToShow.addTrack(audioTracks[i]);
 
-        const videoPlayer = document.createElement('video');
+        //const videoPlayer = document.createElement('video');
         //videoPlayer.setAttribute('controls', '');
         // videoPlayer.setAttribute('width', '320');
         // videoPlayer.setAttribute('height', '240');
 
         // Set the source of the video player.
-        remoteViewPlayer.srcObject = streamToShow;
         // Append the video player to arena
-        remoteArena.appendChild(videoPlayer);
-        videoPlayer.srcObject= streamToShow;
+       
+    if(videoTracks.length>=2){
+        const streamTwoToShow = new MediaStream();
+        streamTwoToShow.addTrack(videoTracks[videoTracks.length-2]);
+        //streamToShow.addTrack(audioTracks[i]);
+        remoteViewTwoPlayer.srcObject= streamTwoToShow;
+
+    }
 
 }
